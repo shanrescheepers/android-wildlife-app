00c524528a1c5378cf66d7042f806f924dcd8904
Shanre Scheepers <21100387@virtualwindow.co.za>
2022-08-16 16:38:54 +0200

    init

---
+++ WildlifeTrivia/app/src/main/java/com/dv202/wildlifetrivia/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dv202.wildlifetrivia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
