c48d3acd4aeaf0a33a29f8b3da67ca8fff946a4d
Shanre Scheepers <21100387@virtualwindow.co.za>
2022-08-16 18:25:34 +0200

    Start page done

---
+++ WildlifeTrivia/app/src/main/java/com/dv202/wildlifetrivia/CategoriesFragment.kt
@@ -0,0 +1,17 @@
+package com.dv202.wildlifetrivia
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class CategoriesFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_categories, container, false)
+    }
+}
